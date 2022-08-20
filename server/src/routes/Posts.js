40920c0c2e1053d3708ca866a5fe95062fc616a2
hoangminhanh-github <hoangminhanh-github>
2022-08-20 15:30:40 +0700

    create-user

---
+++ server/src/routes/Posts.js
@@ -0,0 +1,21 @@
+// const express = require('express')
+// const db = require('../models/index')
+// const router = express.Router()
+// const uuid = require('uuid')
+// router.post('/create', function (req, res, next) {
+//   const post = req.query
+//   db.User.create(post)
+//     .then((hehe) => {
+//       res.json(hehe)
+//     })
+//     .catch(() => {
+//       console.log((create = 'err'))
+//     })
+// })
+
+// router.get('/', function (req, res, next) {
+//   db.User.findAll({ firstName: 'fsdfdfds' }).then((data) => {
+//     res.json(data)
+//   })
+// })
+// module.exports = router
--- src/routes/news.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const newsController = require('../app/controller/NewsControllers')
-
-router.use('/:slug', newsController.show)
-router.use('/', newsController.index)
-
-module.exports = router
